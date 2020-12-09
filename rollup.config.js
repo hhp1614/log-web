1fc7283a2623d1d151ebd13ef9aed9ebd40828b7
hhp1614 <hhp1614@qq.com>
2020-12-09 16:20:04 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,25 @@ import pkg from './package.json';
 
 export default {
   input: 'src/main.ts', // 入口文件
-  output: {
-    file: pkg.main,
-    format: 'umd',
-    name: pkg.webName, // umd 模块名称，相当于一个命名空间，会自动挂载到 window 下面
-    sourcemap: false,
-    plugins: [terser()]
-  },
+  output: [
+    {
+      file: pkg.main, // 输出文件名称
+      format: 'cjs', // 输出模块格式
+      sourcemap: false // 是否输出 sourcemap
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: false
+    },
+    {
+      file: pkg.web,
+      format: 'umd',
+      name: pkg.webName, // umd 模块名称，相当于一个命名空间，会自动挂载到 window 下面
+      sourcemap: false,
+      plugins: [terser()]
+    }
+  ],
   plugins: [
     typescript({
       tsconfigOverride: {
